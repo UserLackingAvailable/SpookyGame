fa944be213cf8b89f6bef290c24da1e9044b7938
Brock <jmbrock@student.uiwtx.edu>
2025-10-28 14:20:35 -0500

    EnemeyAI script added LineOfSight

--- Assets/_Script/Enemy/SEnemyAI.cs
+++ Assets/_Script/Enemy/SEnemyAI.cs
@@ -1,10 +1,14 @@
 using System;
 using UnityEngine;
-using UnityEngine.AI; 
 
 public class SEnemyAI : MonoBehaviour
 {
-    GameObject Target { get {  return Target; } set { Target = value; } }
+    GameObject mTarget;
+    GameObject Target { get { return mTarget; } set { mTarget = value; } }
+
+    [SerializeField] float mEyeHeight = 3f;
+    [SerializeField] float mSightDistance = 5f;
+    [SerializeField] float mViewAngle = 30f;
 
     private void Update()
     {
@@ -14,18 +18,54 @@ public class SEnemyAI : MonoBehaviour
     private void UpdatePlayerPerception()
     {
         SPlayer player = SGameMode.MainGameMode.mPlayer;
-        if(!player)
+        if (!player)
         {
-            Debug.Log("Target not found");
+            
             Target = null;
             return;
         }
 
 
-        //if (Vector3.Distance(player.transform.position, transform.position) > mSightDistance);
-        //{
-        //    Target = null;
-        //    return;
-        //}
+        if (Vector3.Distance(player.transform.position, transform.position) > mSightDistance)
+        {
+            
+            Target = null;
+            return;
+        }
+
+        Vector3 playerDir = (player.transform.position - transform.position).normalized;
+        if (Vector3.Angle(playerDir, transform.forward) > mViewAngle)
+        {
+            
+            Target = null;
+            return;
+        }
+
+        Vector3 eyeViewPoint = transform.position + Vector3.up * mEyeHeight;
+        if (Physics.Raycast(eyeViewPoint, playerDir, out RaycastHit hitInfo, mSightDistance))
+        {
+            if (hitInfo.collider.gameObject != player)
+            {
+                Debug.Log("Object Blocking player");
+                Target = null;
+                return;
+            }
+        }
+
+        Target = player.gameObject;
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 eyeViewPoint = transform.position + Vector3.up * mEyeHeight;
+        Gizmos.DrawWireSphere(eyeViewPoint, mSightDistance);
+
+        Vector3 leftLineDir = Quaternion.AngleAxis(mViewAngle, Vector3.up) * transform.forward;
+        Vector3 rightLineDir = Quaternion.AngleAxis(-mViewAngle, Vector3.up) * transform.forward;
+
+        Gizmos.DrawLine(eyeViewPoint, eyeViewPoint + leftLineDir * mSightDistance);
+        Gizmos.DrawLine(eyeViewPoint, eyeViewPoint + rightLineDir * mSightDistance);
+
+
     }
 }
